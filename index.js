a4acee677a41cf63203560ada5a2350553624f91
Juan Manuel CastillO <juanmcastillo3@gmail.com>
2023-10-10 23:25:38 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
-/*Se desea implementar un sistema de ingreso de torneo competitivo para Age of Empires 2. Se ingresará la cantidad de equipos y se
-solicitará el nombre y elo promedio del equipo para ver si el equipo está apto para participar en el torneo.*/
+/*Se desea implementar un sistema de ingreso de torneo competitivo para Age of Empires 2. En esa ocasión se deberan anotar los equipos.
+calculando el elo promedio del equipo. Luego los organizadores del torneo dictarán si están aptos para
+participar del torneo*/
 
 console.log("Bienvenido a la liga de Corporación AoE");
+alert("Bienvenido a la liga de Corporación AoE");
 
-let cantEquipos = 0;
+function pedirEloApto() {
+  var eloApto = prompt("Ingrese el elo promedio para ingresar al torneo:");
+  return eloApto;
+}
 
-pedirCantEquipos();
+function pedirNombre() {
+  var nombre = prompt("Ingrese el nombre del jugador:");
+  return nombre;
+}
 
-let eloMínimoParaParticipar = prompt(
-  "Indique el elo promedio mínimo para participar"
-);
+function pedirElo() {
+  var elo = parseFloat(prompt("Ingrese la elo del jugador:"));
+  return elo;
+}
 
-hacerSorteo();
 
-function hacerSorteo() {
-  for (let index = 1; index <= cantEquipos; index++) {
-    let nombreelegido = prompt("Ingrese el nombre del equipo " + index);
-    let eloEquipo = prompt("Ingrese el elo del equipo " + index);
-
-    if (eloMínimoParaParticipar >= eloEquipo) {
-      alert("El equipo puede participar");
-    } else {
-      alert("El equipo no puede participar");
-    }
-    console.log(
-      "El equipo " + nombreelegido + " tiene un elo promedio de: " + eloEquipo
-    );
+function calcularPromedio(elos) {
+  var totalElos = 0;
+  for (var i = 0; i < elos.length; i++) {
+    totalElos += elos[i];
   }
+  return totalElos / elos.length;
 }
 
-function pedirCantEquipos() {
-  cantEquipos = prompt(
-    "Ingrese la cantidad de equipos que van a participar del torneo:"
-  );
+
+var jugadoresNombres = [];
+var jugadoresElos = [];
+
+
+var continuar = true;
+
+while (continuar) {
+  var nombre = pedirNombre();
+  var elo = pedirElo();
+
+  if (!isNaN(elo)) {
+    jugadoresNombres.push(nombre);
+    jugadoresElos.push(elo);
+  } else {
+    alert("El elo ingresado no es válido. Intente de nuevo.");
+  }
+
+  var consulta = prompt("¿Desea agregar otro jugador? (Sí/No)").toLowerCase();
+  continuar = consulta === "si" || consulta === "sí";
 }
+
+var eloParaParticipar =  pedirEloApto();
+
+var promedio = calcularPromedio(jugadoresElos);
+
+var mensaje = "Lista de jugadores y elos:\n\n";
+
+for (var i = 0; i < jugadoresNombres.length; i++) {
+  mensaje += jugadoresNombres[i] + ": " + jugadoresElos[i] + "\n";
+}
+
+mensaje += "\nPromedio de elo: " + promedio;
+alert(mensaje);
+
+
+  if (promedio >= eloParaParticipar) {
+    alert("El equipo está apto para jugar el torneo");
+  } else{
+    alert("El equipo no está apto para jugar el torneo")
+  }
